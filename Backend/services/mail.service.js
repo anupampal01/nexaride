5a5db3c2e5f72bbe8569f4c96e19fa210fb78160
Anupam . <anupampal1307@gmail.com>
2026-04-16 01:32:49 +0530

    Updated dependencies and clean reinstall fix

--- Backend/services/mail.service.js
+++ Backend/services/mail.service.js
@@ -3,14 +3,19 @@ const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendMail = async (to, subject, html) => {
-  const response = await resend.emails.send({
-    from: "onboarding@resend.dev",
-    to,
-    subject,
-    html,
-  });
+  try {
+    const response = await resend.emails.send({
+      from: "anupampal1307@gmail.com",
+      to,
+      subject,
+      html,
+    });
 
-  return response;
+    return response;
+  } catch (error) {
+    console.error("Resend Error:", error);
+    throw error;
+  }
 };
 
 module.exports = sendMail;
