fa64e4d1c34bc7666ce94910d62bc25d1eb4069e
Anupam . <anupampal1307@gmail.com>
2026-04-15 23:08:45 +0530

    Updated dependencies and clean reinstall fix

--- Backend/server.js
+++ Backend/server.js
@@ -30,10 +30,13 @@ if (process.env.ENVIRONMENT == "production") {
 } else {
   app.use(morgan("dev"));
 }
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
